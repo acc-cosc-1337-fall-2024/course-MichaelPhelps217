0dd92edfe32cead7eef1e8a0ef5d6f18f50fd67a
MichaelPhelps217 <157077931+MichaelPhelps217@users.noreply.github.com>
2024-03-30 21:00:15 +0000

    tic_tac_toe (part 1)

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,31 @@
+#include "tic_tac_toe.h"
 
 int main() 
 {
+	TicTacToe game;
+	std::string first_player;
+	char user_choice = 'y';
+
+	do
+	{
+		std::cout<<"Enter first player: ";
+		std::cin>>first_player;
+
+		game.start_game(first_player);
+		int position;
+
+		while(!game.game_over())
+		{
+			std::cout<<"Enter a position: ";
+			std::cin>>position;
+			game.mark_board(position);
+			game.display_board();
+		}
+
+		std::cout<<"Play Again (y or n)? ";
+		std::cin>>user_choice;
+
+	}while(user_choice == 'y' || user_choice == 'Y');
 
 	return 0;
 }
